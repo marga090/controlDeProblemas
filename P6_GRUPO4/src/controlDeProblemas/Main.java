5b1803e4ea3e8b2c44405157d95d3e2fa837e025
marga090 <mgpedregal@gmail.com>
2024-02-11 20:08:34 +0100

    controlDeProblemas v1

---
+++ P6_GRUPO4/src/controlDeProblemas/Main.java
@@ -0,0 +1,10 @@
+package controlDeProblemas;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ P6_GRUPO4/src/controlDeProblemas/Tarea.java
@@ -0,0 +1,5 @@
+package controlDeProblemas;
+
+public class Tarea {
+
+}
