812dc5c1609cd6848772ebb31dabfd1a638bdb7e
AdrianMorGar <159542087+AdrianMorGar@users.noreply.github.com>
2024-02-13 12:02:23 +0100

    Update Tarea.java

--- P6_GRUPO4/src/controlDeProblemas/Tarea.java
+++ P6_GRUPO4/src/controlDeProblemas/Tarea.java
@@ -1,5 +1,19 @@
 package controlDeProblemas;
 
 public class Tarea {
+    private Problema problema;
+    private int numeroOrdenador;
 
+    public Tarea(Problema problema, int numeroOrdenador) {
+        this.problema = problema;
+        this.numeroOrdenador = numeroOrdenador;
+    }
+
+    public Problema getProblema() {
+        return problema;
+    }
+
+    public int getNumeroOrdenador() {
+        return numeroOrdenador;
+    }
 }
