7e788f76b32350580afae5164e32ad4d92f53ab0
AdrianMorGar <159542087+AdrianMorGar@users.noreply.github.com>
2024-02-20 14:46:48 +0100

    modif TareaV1.java

--- P6_GRUPO4/src/controlDeProblemas/Tarea.java
+++ P6_GRUPO4/src/controlDeProblemas/Tarea.java
@@ -1,15 +1,96 @@
-package controlDeProblemas;
+package controlDeProblemas.consultarProblema;
+
+import java.util.Date;
+
+import controlDeProblemas.consultarProblema.enumerados.EstadoOrdenador;
+import controlDeProblemas.users.Tecnico;
 
 public class Tarea {
+	private int id;
+	private String descripcion;
+	private Tecnico tecnico;
+	private int ordenador;
+	private EstadoOrdenador estado;
+	private String solucion;
+	private Date fecha;
 
-	private int numeroOrdenador;
+	static int contId = 0;
 
-	public Tarea(int numeroOrdenador) {
-
-		this.numeroOrdenador = numeroOrdenador;
+	// costructor
+	
+	public Tarea(Tecnico tecnico, int ordenador, String descripcion) {
+		this.id = this.contId + 1;
+		this.descripcion = descripcion;
+		this.tecnico = null;
+		this.ordenador = ordenador;
+		this.estado = EstadoOrdenador.PENDIENTE;
+		this.contId++;
 	}
 
-	public int getNumeroOrdenador() {
-		return numeroOrdenador;
+	// metodo
+
+	public void resolverTarea(Tecnico tecnico,EstadoOrdenador estado, String solucion) {
+		this.tecnico = tecnico;
+		this.solucion = solucion;
+		this.estado = EstadoOrdenador.RESUELTA;
+		this.fecha = new Date();
+	}
+
+// geter y seter
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Tecnico getTecnico() {
+		return tecnico;
+	}
+
+	public void setTecnico(Tecnico tecnico) {
+		this.tecnico = tecnico;
+	}
+
+	public int getOrdenador() {
+		return ordenador;
+	}
+
+	public void setOrdenador(int ordenador) {
+		this.ordenador = ordenador;
+	}
+
+	public EstadoOrdenador getEstado() {
+		return estado;
+	}
+
+	public void setEstado(EstadoOrdenador estado) {
+		this.estado = estado;
+	}
+
+	public String getSolucion() {
+		return solucion;
+	}
+
+	public void setSolucion(String solucion) {
+		this.solucion = solucion;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
 	}
 }
