deab91f1194708d8622663be6a8890ff650d3d1b
AdrianMorGar <159542087+AdrianMorGar@users.noreply.github.com>
2024-02-22 19:44:38 +0100

    Pruebas V1 Main.java

--- P6_GRUPO4/src/controlDeProblemas/Main.java
+++ P6_GRUPO4/src/controlDeProblemas/Main.java
@@ -1,10 +1,34 @@
 package controlDeProblemas;
 
 public class Main {
+    public static void main(String[] args) {
+        // Crear un técnico
+        Tecnico tecnico = new Tecnico("Juan", "Perez", 1);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        // Crear una tarea
+        Tarea tarea = new Tarea(123, "El ordenador no enciende.");
 
-	}
+        // Imprimir información de la tarea antes de resolverla
+        System.out.println("Información del problema");
+        System.out.println("----------------------------------");
+        System.out.println("ID: " + tarea.getId());
+        System.out.println("Descripción: " + tarea.getDescripcion());
+        System.out.println("Ordenador: " + tarea.getOrdenador());
+        System.out.println("Estado: " + tarea.getEstado());
+        System.out.println("*****************************************************");
 
+        // Resolver la tarea
+        tarea.resolverTarea(tecnico, "El problema era un cable suelto.");
+
+        // Imprimir información de la tarea después de resolverla
+        System.out.println("Información del problema");
+        System.out.println("----------------------------------");
+        System.out.println("ID: " + tarea.getId());
+        System.out.println("Descripción: " + tarea.getDescripcion());
+        System.out.println("Técnico asignado: " + tarea.getTecnico().getNombre() + " " + tarea.getTecnico().getApellidos());
+        System.out.println("Ordenador: " + tarea.getOrdenador());
+        System.out.println("Estado: " + tarea.getEstado());
+        System.out.println("Solución: " + tarea.getSolucion());
+        System.out.println("Fecha de resolución: " + tarea.getFecha());
+    }
 }
